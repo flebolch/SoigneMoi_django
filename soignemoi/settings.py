137d2bf6ad92f1618390fb3cd40bb4ac9cb40967
flebolch <flebolch@users.noreply.github.com>
2024-01-18 04:04:31 +0100

    masquage de la clé

--- soignemoi/settings.py
+++ soignemoi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j6j=hepc2&dqpn$!109kpd#--xut%4*0w(qi2rm_e#!!b+pp7e'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
