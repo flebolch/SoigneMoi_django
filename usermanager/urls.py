271a81cbe1e0921530dbd6e002b39ec84e476cc0
flebolch <flebolch@users.noreply.github.com>
2024-01-23 05:31:22 +0100

    login new users redirection issue

--- usermanager/urls.py
+++ usermanager/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('nouveau_patient/', views.register, name='nouveau_patient'),
-    # path('patient_info/', views.login, name='patient_info'),
     path('connection/', views.login, name='connection'),
+    path('patient_info/', views.edit_profile, name='patient_info'),
+    path('mon_espace_visiteur/', views.visitordashboard, name='mon_espace_visiteur'),
     path('deconnection/', views.logout, name='deconnection'),
 ]
--- usermanager/views.py
+++ usermanager/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms.registerationforms import RegistrationForm, PatientProfileForm
 from .models import Account, PatientProfile
-from django.contrib.auth import authenticate
+from django.contrib import messages, auth
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def register (request):
@@ -13,6 +14,7 @@ def register (request):
            last_name = form.cleaned_data['last_name']
            password = form.cleaned_data['password']
            user = Account.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password)
+           user.is_active = True
            print(user)
            user.save()
            PatientProfile.objects.create(user=user)
@@ -30,21 +32,53 @@ def register (request):
 
 def login (request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
+        username = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
         
         if user is not None:
-            if hasattr(user, 'patientprofile'):
-                login(request, user)
-                return redirect('patient_home')
+            if user.is_active:
+                print("Utilisateur actif")
+            print("Authentification réussie")
+            auth.login(request, user)
+            if hasattr(user, 'patientprofile'):  # Check if the user has a PatientProfile
+                if user.patientprofile.address_line_1 == '':  # Check if address_line_1 is empty
+                    return edit_profile(request)
+                else:
+                    return visitordashboard(request)
             else:
-                login(request, user)
-                return redirect(request,'patient_info.html')
+                print("User does not have a PatientProfile")
+                return redirect('visitordashboard')
+        else:
+            print("Problème d'authentification")
     else:
-            return render(request, 'usermanager/login.html', {'form': PatientProfileForm})
-            
+        print("Problème de méthode")
     return render(request, 'usermanager/login.html')
+
+def edit_profile(request):
+    # userprofile = get_object_or_404(UserProfile, user=request.user)
+    # if request.method == 'POST':
+    #     user_form = UserForm(request.POST, instance=request.user)
+    #     profile_form = UserProfileForm(request.POST, request.FILES, instance=userprofile)
+    #     if user_form.is_valid() and profile_form.is_valid():
+    #         user_form.save()
+    #         profile_form.save()
+    #         messages.success(request, 'Your profile has been updated.')
+    #         return redirect('edit_profile')
+    # else:
+    #     user_form = UserForm(instance=request.user)
+    #     profile_form = UserProfileForm(instance=userprofile)
+    # context = {
+    #     'user_form': user_form,
+    #     'profile_form': profile_form,
+    #     'userprofile': userprofile,
+    # }
+    return render(request, 'usermanager/patient_info.html')
+
+
+def visitordashboard (request):
+    return render(request, 'usermanager/mon_espace_visiteur.html')
+
 
 def logout (request):
     return
