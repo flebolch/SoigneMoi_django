ba0dff7f38c876efd60746801b026d80aee94c26
flebolch <flebolch@users.noreply.github.com>
2024-01-23 21:04:06 +0100

    dbname udapate and static cleaned

--- soignemoi/settings.py
+++ soignemoi/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'soignemoi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'soignemoi.sqlite3',
     }
 }
 
