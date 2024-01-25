03bf55ba2d4dc8dc9f3c7403972f406116e159b4
flebolch <flebolch@users.noreply.github.com>
2024-01-25 19:14:09 +0100

    homepage with details hide and ling for outstide

--- soignemoi/settings.py
+++ soignemoi/settings.py
@@ -42,9 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_htmx',
     'homepage',
-    'service',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = 'soignemoi.urls'
