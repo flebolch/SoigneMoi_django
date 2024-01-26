482819bb439940fed3db125fdd6b93d9285a2be8
flebolch <flebolch@users.noreply.github.com>
2024-01-26 10:23:07 +0100

    filter on appointments

--- appointment/views.py
+++ appointment/views.py
@@ -7,8 +7,12 @@ def patient_appointments(request, patient_id):
     all_appointments = Appointment.objects.filter(patient=patient)
 
     past_appointments = all_appointments.filter(date_stop__lt=timezone.now())
-    future_appointments = all_appointments.exclude(date_stop__lt=timezone.now())
-    current_appointments = future_appointments.filter(date_start__lt=timezone.now(), date_stop__gt=timezone.now())
+    future_appointments = all_appointments.filter(date_start__gt=timezone.now())
+    current_appointments = all_appointments.filter(date_start__lte=timezone.now(), date_stop__gte=timezone.now())
+    print('all:', all_appointments)
+    print('past:', past_appointments)
+    print('future:', future_appointments)
+    print('current:', current_appointments)
 
     context = {
         'patient': patient,
