ab140ec3b3f01fbefacb04f77679e438b6cc344b
flebolch <flebolch@users.noreply.github.com>
2024-02-21 08:15:57 +0100

    adding django-admin-honeypot-updated-2021

--- soignemoi/urls.py
+++ soignemoi/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', include('admin_honeypot.urls')),
+    path('securelogin/', admin.site.urls),
     path('',views.home, name='home'),
     path('', include('homepage.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
